e5dc868035eb966bc17783b108b26d2a0aae8b49
jonah3d <84209915+jonah3d@users.noreply.github.com>
2025-06-07 02:07:39 +0200

    lets goo

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  // This tells Vite that your site will be in a subfolder called /my-blog/
+  base: '/my-blog/',
+  plugins: [react()],
 })
